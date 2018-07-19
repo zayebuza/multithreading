f6337e6ab0fc430c1fd149b241a50da16531185f
Shanks <24303369@qq.com>
2018-07-19 23:15:09 +0800

    init project

---
+++ src/main/java/com/example/multithreading/MultithreadingApplication.java
@@ -0,0 +1,12 @@
+package com.example.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingApplication.class, args);
+	}
+}
